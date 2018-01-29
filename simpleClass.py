b37323e95a328f015464bd66c06e06d725a54529
zeng liang <zenliang@foxmail.com>
2018-01-30 00:14:35 +0800

    Update simpleClass.py

--- simpleClass.py
+++ simpleClass.py
@@ -1,4 +1,4 @@
-
+#__enter__ , __exit__ is to support "with" statement, they are optional
 
 class CountFromBy():
     def __init__(self, start:int = 0, step:int = 1):
@@ -8,6 +8,12 @@ class CountFromBy():
     def increase(self) -> None:
         self.num += self.step
 
+    def __enter__(self) -> None:        
+        pass
+
+    def __exit__(self) -> None:
+        pass        
+        
 if __name__ == '__main__':
     c = CountFromBy(step=15)
     for i in range(3):
