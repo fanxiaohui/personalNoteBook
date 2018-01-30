b1e16256cfc98b46499d283632a7154f0a48ccf0
zeng liang <zenliang@foxmail.com>
2018-01-30 22:01:34 +0800

    Create pythonLearn-decorator.py

---
+++ pythonLearn-decorator.py
@@ -0,0 +1,19 @@
+
+
+def apply(func: object, *value) -> object:
+    return func(*value)
+
+
+def outer(*args) -> object:
+    def inner(word:str):
+        print(*args, word)    
+    return inner
+    
+
+if __name__ == '__main__':    
+    f = apply(outer,1,2)
+    f('hello')
+    
+    
+///////////output:
+1 2 hello
