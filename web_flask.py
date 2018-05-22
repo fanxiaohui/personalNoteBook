3fc51c7ec6a6bea1e342518eaeb3013d62f83dc6
ZL <zenliang@foxmail.com>
2018-05-22 23:56:03 +0800

    Update web_flask.py

--- web_flask.py
+++ web_flask.py
@@ -1,16 +1,15 @@
-
 # -*- coding: utf-8 -*-
 """
 Created on Tue May  1 15:19:36 2018
 
-python web, it worked.
+python web, it worked. run this file in spyder, then open url in browser: http://127.0.0.1:5000/, this url could be got from the running log.
 
-open url in browser:   http://127.0.0.1:5000/
+dependency:there are 3 other html files(base.html,entry.html,results.html) together with this py file.
 
 @author: lianzeng
 """
 
-from flask import Flask
+from flask import Flask,render_template,request,redirect
 
 
 def search4Letters(phrase:str, letters:str):
@@ -22,11 +21,23 @@ def search4Letters(phrase:str, letters:str):
 app = Flask(__name__)
 
 @app.route('/')
-def hello()->str:
-    return 'Great ! hello from flask'
+def hello()->'302':
+    return redirect('/entry')
 
-@app.route('/search4')
-def do_search()->str:
-    return str(search4Letters('i am learning web','aeiou'))
+@app.route('/search4',methods=['POST'])
+def do_search()->'html':
+    phrase = request.form['phrase']
+    letter = request.form['letters']
+    results =  str(search4Letters(phrase,letter))
+    return render_template('results.html',
+                           the_phrase=phrase,
+                           the_letters=letter,
+                           the_results=results,
+                           the_title = 'Here are results:')
 
-app.run()
+@app.route('/entry')
+def entry_page()->'html':
+    return render_template('entry.html',the_title='Web using Python Flask!')
+
+if __name__ == '__main__':
+    app.run()
